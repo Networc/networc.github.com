3f1aac477761bf44d2828e839c52c876afc23678
Andrew Reed <andrew.reed@west-point.org>
2012-08-27 15:52:10 -0400

    GitHub API v3

--- javascripts/jquery.github.js
+++ javascripts/jquery.github.js
@@ -16,6 +16,8 @@ jQuery.fn.loadRepositories = function(username) {
             if (this.name != (username.toLowerCase()+'.github.com')) {
                 list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
                 list.append('<dd>' + this.description +'</dd>');
+		list.append('<dd><em>Size: '+(this.size<1000?(this.size+' kB'):(Math.round((this.size/1000)*100)/100+' MB'))+' - Watchers: '+this.watchers_count+' - Forks: '+this.forks_count+' </em></dd>');
+		list.append('<dd><br/></dd>');
             }
         });      
       });
